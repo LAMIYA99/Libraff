89a2be36a9ea5851cdefb5df69c9a7fd21f68042
Marvan S. Mammadli <marvansm@code.local>
2026-01-24 16:04:40 +0400

    fix: fixed import urls

--- frontend/src/components/sections/home/TodayChoicesSection.tsx
+++ frontend/src/components/sections/home/TodayChoicesSection.tsx
@@ -7,13 +7,7 @@ const TodayChoicesSection = () => {
     <div>
       <SectionHeading title="Bugünün seçimləri" />
       <LanguageFilter />
-      <div className="grid grid-cols-6 gap-1">
-        <BookCard
-          image="https://www.libraff.az/images/thumbnails/400/600/from_1c/f8a032c0-5053-11eb-a4d2-503eaa128442_1_1759181531.jpg.webp"
-          title="test"
-          price={199.99}
-          disCountPrice={99.99}
-        />
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
         <BookCard
           image="https://www.libraff.az/images/thumbnails/400/600/from_1c/f8a032c0-5053-11eb-a4d2-503eaa128442_1_1759181531.jpg.webp"
           title="test"
